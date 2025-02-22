14a7f6fefa381e46dcab2c1a9701bdb5480aa897
tonay11 <toni.ayon@proton.me>
2025-02-22 23:42:34 +0000

    mytext

--- session01/daz ds session 1 task 1 .py
+++ session01/daz ds session 1 task 1 .py
@@ -6,4 +6,11 @@ print (hobbies)
 mySeparator = ". "
 my_text = 'My name is '+ my_name + mySeparator + 'I live in ' + city + mySeparator + 'My hobbies are ' + hobbies
 print (my_text)
-f = open("daz ds session 1 task 1.py", "t" )
+f = open ("daz ds session 1 task 1.txt", "w")
+f = open("daz ds session 1 task 1.txt", "a" )
+f.write("my_text")
+f.close()
+
+#open and read the file after the appending:
+f = open("daz ds session 1 task 1.txt", "r")
+print(f.read())
---
+++ session01/daz session 1.py
@@ -0,0 +1,13 @@
+#task 2
+a = 12
+b = 3
+if a % b == 0:
+   print ("Even")
+
+
+def odd_even (e):
+   e = a % b
+   a = 12
+   b = 3
+   if (e == 0):
+      print ("Even")
